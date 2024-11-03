4cd32b98b813c93c1d6f87d0c3165a2e51d259c7
Glockrover <ayandadlokovich@icloud.com>
2024-11-03 19:05:31 +0200

    Done

---
+++ Day13/problem.py
@@ -0,0 +1,19 @@
+def longest_zero_sum_subarray(nums):
+    if nums == []:
+        return list(nums)
+    else:
+        array = []
+        for number in range(len(nums)):
+            for i in range(number + 1, len(nums) + 1):
+                array.append(nums[number:i])
+        
+        new = list()
+        for numbers in array:
+            if sum(numbers) == 0:
+                new.append(numbers)
+        if new:
+            return new[0]
+        else:
+            return []
+
+print(longest_zero_sum_subarray([1, 2, -3, 3, -3, 3]))
