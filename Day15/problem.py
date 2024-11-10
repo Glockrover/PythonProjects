8645f730cf231cd5d7e93211e84e4a39a099c760
Glockrover <ayandadlokovich@icloud.com>
2024-11-10 09:17:21 +0200

    One more test to pass

--- Day15/problem.py
+++ Day15/problem.py
@@ -1,9 +1,10 @@
 def is_anagram_of_palindrome(s):
-    # Your code for checking anagram of palindrome goes here
+    if s != s[::-1] and sorted(s) != sorted(s):
+        return False
     if s == s[::-1]:
-        return s
+        return True
     elif s == "":
         return True
-    elif sorted(s) == s:
-        return s
+    elif sorted(s) == sorted(s):
+        return True
     
