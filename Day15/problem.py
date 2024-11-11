98eb335f7269253e8bf9e8ade048e2531b783079
Glockrover <ayandadlokovich@icloud.com>
2024-11-11 21:18:56 +0200

    Done

--- Day15/problem.py
+++ Day15/problem.py
@@ -1,10 +1,18 @@
 def is_anagram_of_palindrome(s):
-    if s != s[::-1] and sorted(s) != sorted(s):
-        return False
     if s == s[::-1]:
         return True
-    elif s == "":
-        return True
-    elif sorted(s) == sorted(s):
-        return True
     
+    else:
+        letter = 0
+        count_letters = [s.count(x) for x in s]
+        
+        for i in count_letters:
+            if i == 1:
+                letter += 1
+        
+        if letter > 1:
+            return False
+        else:
+            return True
+            
+print(is_anagram_of_palindrome("Hello"))
